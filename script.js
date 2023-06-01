cfa1740b75baf951bbb85e026dc193ef3fa0559e
bentike <ajadiyusuf73@gmail.com>
2023-06-01 21:06:32 +0100

    selected overlay items

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const firstName = document.getElementById("firstName");
 const middleName = document.getElementById("middleName");
-const lastNme = document.getElementById("lastName");
+const lastName = document.getElementById("lastName");
 const dob = document.getElementById("dob");
 const studentId = document.getElementById("studentId");
 const streetAddress = document.getElementById("streetAddress");
@@ -15,3 +15,31 @@ const level = document.getElementById("level");
 const language = document.getElementById("language");
 const startDate = document.getElementById("start");
 const comment = document.getElementById("comment");
+const submit = document.getElementById("submit");
+const close = document.getElementById("close");
+const preview = document.getElementsByClassName("preview")[0];
+const overlayFirstName = document.getElementById("fn");
+const overlayMiddleName = document.getElementById("mn");
+const overlayLastName = document.getElementById("ln");
+const overlayBob = document.getElementById("dofb");
+const overlayStudentId = document.getElementById("sid");
+const overlayStreetAddress = document.getElementById("sa");
+const overlayCity = document.getElementById("ct");
+const overlayState = document.getElementById("st");
+const overlayCountry = document.getElementById("cty");
+const overlayZipCode = document.getElementById("zc");
+const overlayEmail = document.getElementById("eml");
+const overlayNumber = document.getElementById("num");
+const overlayCourse = document.getElementById("crs");
+const overlayLevel = document.getElementById("lv");
+const overlayLanguage = document.getElementById("lng");
+const overlayStartDate = document.getElementById("std");
+const overlayComment = document.getElementById("cmt");
+
+submit.addEventListener("click", (event) => {
+    preview.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+    preview.style.display = "none";
+});
